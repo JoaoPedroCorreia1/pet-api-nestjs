0e19ad95d2d48d78b805a2c1ca2a75c495aee77e
JoaoPedroCorreia1 <jpmourasilva@gmail.com>
2025-03-14 23:03:56 -0300

    feat: add backend service to Docker

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-// src/main.ts
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
@@ -18,6 +16,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(3333);
 }
 bootstrap();
